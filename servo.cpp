16dbfac186fa8f14aed45ff94e3d13a6ff528328
t_rappos <t_rappos@hotmail.com>
2016-05-13 12:30:37 +1000

    (UNTESTED) added ADC reading ability (MCP3004/8), servo position now readable

--- servo.cpp
+++ servo.cpp
@@ -1,5 +1,6 @@
 #include "servo.h"
 #include "PCA9685.h" //from https://github.com/TeraHz/PCA9685
+#include <mcp3004.h>
 
 PCA9685 * servo::_servo_controller;
 
@@ -8,6 +9,7 @@ void servo::Initialise()
 	_servo_controller = 0;
 	_servo_controller = new PCA9685(SERVO_BUS,SERVO_ADDRESS);
 	_servo_controller->setPWMFreq(SERVO_DEFAULT_FREQ);
+	mcp3004Setup(ADC_BASE,ADC_SPI_CHANNEL);
 }
 
 void servo::Finalise()
@@ -31,4 +33,28 @@ void servo::SetPos(float percentage_open)
 	float v = (float)SERVO_CLOSED_PWM_VALUE +
 			(((float)SERVO_OPEN_PWM_VALUE - (float)SERVO_CLOSED_PWM_VALUE)  * percentage_open);
 	_servo_controller->setPWM(SERVO_CHANNEL,v);
+}
+
+static int servo::GetAnalogInput(int adc_channel)
+{
+	int result = analogRead(ADC_BASE + adc_channel)
+	return result;
+}
+
+static int servo::GetServoPos()
+{
+	int result = GetAnalogInput(ADC_POT_CHANNEL);
+	return result;
+}
+
+bool servo::IsOpen()
+{
+	bool result = GetServoPos() > SERVO_OPEN_THRESHOLD;
+	return result;
+}
+	
+bool servo::IsClosed()
+{
+	bool result = GetServoPos() < SERVO_CLOSED_THRESHOLD;
+	return result;
 }
--- servo.h
+++ servo.h
@@ -15,13 +15,27 @@ be included in the project settings somewhere like you would normally include
 a library. But this works in the meantime
 */
 
+/*
+ADC NOTES:
+run terminal commands below to test if the ADC is wired properly
+gpio load spi
+
+Note: not sure which will work, some combination of both of the following lines...
+gpio -x mcp3004:100:0 aread 100
+gpio -x gpio -x mcp3008:200:0 aread 200
+*/
 
 #define SERVO_BUS (0)
 #define SERVO_ADDRESS (0x40)
 #define SERVO_CHANNEL (0)
+#define ADC_BASE (100)				//TODO: Check over these
+#define ADC_SPI_CHANNEL (0)
+#define ADC_POT_CHANNEL (0)         //the adc pin/channel the servo pot is connected to?
 #define SERVO_DEFAULT_FREQ (60)
 #define SERVO_CLOSED_PWM_VALUE (0)
 #define SERVO_OPEN_PWM_VALUE (4095)     // TODO check if these tick values are correct for our motor!
+#define SERVO_CLOSED_THRESHOLD (100)
+#define SERVO_OPEN_THRESHOLD (1024-100)
 
 class PCA9685;
 
@@ -45,6 +59,18 @@ public:
 
     //set the percentage the servo is fully open, valid value between [0.0,1.0]
     static void SetPos(float percentage_open);
+	
+	//returns the position of the servo [0,1024]
+	static int GetServoPos();
+	
+	//returns the value of an adc input channel [0,1024]
+	static int GetAnalogInput(int adc_channel);
+	
+	//returns if the servo is open
+	static bool IsOpen();
+	
+	//returns if the servo is closed
+	static bool IsClosed();
 };
 
 
